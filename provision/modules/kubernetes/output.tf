1477733b069bfa1b60086a9fa066c9cdaf57b6d5
root <cristiano.rosetti@gmail.com>
2023-11-02 21:33:54 +0000

    prometheus

--- provision/modules/kubernetes/output.tf
+++ provision/modules/kubernetes/output.tf
@@ -5,3 +5,10 @@ output "service_account_token" {
 output "nginx_ingress" {
   value = data.kubernetes_service.nginx_ingress.status.0.load_balancer.0.ingress[0].ip
 }
+
+output "helm-charts" {
+  value = [
+    "nginx-ingress",
+    "prometheus"
+  ]
+}
