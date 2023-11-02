6a927688abd896db409b2088ac540a2856dc683e
root <cristiano.rosetti@gmail.com>
2023-11-02 22:00:46 +0000

    prometheus

--- provision/modules/kubernetes/prometheus.tf
+++ provision/modules/kubernetes/prometheus.tf
@@ -1,9 +1,9 @@
-# resource "helm_release" "prometheus" {
-#   name       = "prometheus"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart      = "kube-prometheus-stack"
-#   version    = "52.1.0"
-# }
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "52.1.0"
+}
 
 # data "kubernetes_service" "prometheus_ingress" {
 #   depends_on = [resource.helm_release.prometheus]
