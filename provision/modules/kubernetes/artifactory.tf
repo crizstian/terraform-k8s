b7bde913db39dd2a71c7e10a9a3cec5dc83636e3
root <cristiano.rosetti@gmail.com>
2023-11-09 04:11:26 +0000

    prometheus

--- provision/modules/kubernetes/artifactory.tf
+++ provision/modules/kubernetes/artifactory.tf
@@ -1,18 +1,18 @@
-resource "helm_release" "artifactory" {
-  name       = "artifactory"
-  repository = "https://charts.jfrog.io"
-  chart      = "artifactory-oss"
-  version    = "107.71.3"
-  set {
-    name  = "artifactory.nginx.enabled"
-    value = "true"
-  }
+# resource "helm_release" "artifactory" {
+#   name       = "artifactory"
+#   repository = "https://charts.jfrog.io"
+#   chart      = "artifactory-oss"
+#   version    = "107.71.3"
+#   set {
+#     name  = "artifactory.nginx.enabled"
+#     value = "true"
+#   }
 
-  set {
-    name  = "artifactory.ingress.enabled"
-    value = "false"
-  }
-}
+#   set {
+#     name  = "artifactory.ingress.enabled"
+#     value = "false"
+#   }
+# }
 
 # data "kubernetes_service" "prometheus_ingress" {
 #   depends_on = [resource.helm_release.prometheus]
