0aae9e630d6e8741f097efdd4f0bec2a7ce0cbf0
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-15 13:33:34 -0600

    Update output.tf

--- provision/modules/kubernetes/output.tf
+++ provision/modules/kubernetes/output.tf
@@ -2,13 +2,13 @@ output "service_account_token" {
   value = kubernetes_secret_v1.sa
 }
 
-output "nginx_ingress" {
-  value = data.kubernetes_service.nginx_ingress.status.0.load_balancer.0.ingress[0].ip
-}
+#output "nginx_ingress" {
+#  value = data.kubernetes_service.nginx_ingress.status.0.load_balancer.0.ingress[0].ip
+#}
 
-output "helm-charts" {
-  value = [
-    "nginx-ingress",
-    "prometheus"
-  ]
-}
+#output "helm-charts" {
+#  value = [
+#    "nginx-ingress",
+#    "prometheus"
+#  ]
+#}
