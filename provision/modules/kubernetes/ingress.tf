6e8d162307b7c356df38aae74953d9d3b487f10a
Cristian Ramirez <cristiano.rosetti@gmail.com>
2023-11-15 13:33:09 -0600

    Update ingress.tf

--- provision/modules/kubernetes/ingress.tf
+++ provision/modules/kubernetes/ingress.tf
@@ -1,23 +1,23 @@
-resource "helm_release" "release" {
-  name       = "nginx-ingress"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = "4.8.3"
+#resource "helm_release" "release" {
+#  name       = "nginx-ingress"
+#  repository = "https://kubernetes.github.io/ingress-nginx"
+#  chart      = "ingress-nginx"
+#  version    = "4.8.3"
 
-  set {
-    name  = "controller.publishService.enabled"
-    value = "true"
-  }
+#  set {
+#    name  = "controller.publishService.enabled"
+#    value = "true"
+#  }
 
-  set {
-    name  = "controller.allowSnippetAnnotations"
-    value = "true"
-  }
-}
+#  set {
+#    name  = "controller.allowSnippetAnnotations"
+#    value = "true"
+#  }
+#}
 
-data "kubernetes_service" "nginx_ingress" {
-  depends_on = [resource.helm_release.release]
-  metadata {
-    name = "nginx-ingress-ingress-nginx-controller"
-  }
-}
+#data "kubernetes_service" "nginx_ingress" {
+#  depends_on = [resource.helm_release.release]
+#  metadata {
+#    name = "nginx-ingress-ingress-nginx-controller"
+#  }
+#}
