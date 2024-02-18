bc0a07202412f34f7395aee0a67b70ec9420770f
Cristian Ramirez <cristiano.rosetti@gmail.com>
2024-02-17 20:04:16 -0800

    Update crossplane.tf

--- provision/modules/kubernetes/crossplane.tf
+++ provision/modules/kubernetes/crossplane.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "crossplane" {
   name       = "crossplane"
   repository = "https://charts.crossplane.io/stable"
-  chart      = "crossplane-stable"
+  chart      = "crossplane"
   version    = "1.15.0"
   namespace  = "crossplane-system"
   create_namespace = "true"
