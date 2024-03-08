ac2d86c62a2046aea1f8ac8857cb06080b90c4bb
Cristian Ramirez <cristiano.rosetti@gmail.com>
2024-03-07 20:45:07 -0600

    Update prometheus.tf

--- provision/modules/kubernetes/prometheus.tf
+++ provision/modules/kubernetes/prometheus.tf
@@ -1,13 +1,13 @@
-#resource "helm_release" "prometheus" {
-#  name       = "prometheus"
-#  repository = "https://prometheus-community.github.io/helm-charts"
-#  chart      = "kube-prometheus-stack"
-#  version    = "52.1.0"
-#}
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "52.1.0"
+}
 
-# data "kubernetes_service" "prometheus_ingress" {
-#   depends_on = [resource.helm_release.prometheus]
-#   metadata {
-#     name = "nginx-ingress-ingress-nginx-controller"
-#   }
-# }
+ data "kubernetes_service" "prometheus_ingress" {
+   depends_on = [resource.helm_release.prometheus]
+   metadata {
+     name = "nginx-ingress-ingress-nginx-controller"
+   }
+ }
