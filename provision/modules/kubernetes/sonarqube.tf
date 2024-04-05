f34aecf8f05fbd175ecf7d226d122de7f19c57f1
Cristian Ramirez <cristiano.rosetti@gmail.com>
2024-04-05 11:59:20 -0600

    Update sonarqube.tf

--- provision/modules/kubernetes/sonarqube.tf
+++ provision/modules/kubernetes/sonarqube.tf
@@ -1,1 +1,6 @@
-x
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://SonarSource.github.io/helm-chart-sonarqube"
+  chart      = "sonarqube"
+  version    = "1.29"
+}
