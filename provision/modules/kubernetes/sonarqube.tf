6be1c28a1d1c7914b97e4e45a47a683e58e0bc02
cristian.ramirez@harness.io <cristiano.rosetti@gmail.com>
2024-07-09 20:29:27 -0600

    init container

--- provision/modules/kubernetes/sonarqube.tf
+++ provision/modules/kubernetes/sonarqube.tf
@@ -2,5 +2,4 @@ resource "helm_release" "sonarqube" {
   name       = "sonarqube"
   repository = "https://SonarSource.github.io/helm-chart-sonarqube"
   chart      = "sonarqube"
-  version    = "1.29"
 }
