3e8818e62e2885b78581e8a9088244999c4d64c5
cristian.ramirez@harness.io <cristiano.rosetti@gmail.com>
2024-07-09 20:18:15 -0600

    add

--- provision/modules/kubernetes/prometheus.tf
+++ provision/modules/kubernetes/prometheus.tf
@@ -5,9 +5,9 @@ resource "helm_release" "prometheus" {
   version    = "52.1.0"
 }
 
- data "kubernetes_service" "prometheus_ingress" {
-   depends_on = [resource.helm_release.prometheus]
-   metadata {
-     name = "nginx-ingress-ingress-nginx-controller"
-   }
- }
+#  data "kubernetes_service" "prometheus_ingress" {
+#    depends_on = [resource.helm_release.prometheus]
+#    metadata {
+#      name = "prometheus"
+#    }
+#  }
